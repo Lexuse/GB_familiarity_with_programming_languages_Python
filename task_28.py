1a2fb68705081731fa199a4806a539b35b6f9ec5
Alexey <Lexuse2007@gmail.com>
2023-02-12 12:02:37 +0300

    task_28 finished

---
+++ task_28.py
@@ -0,0 +1,11 @@
+#Напишите рекурсивную функцию sum(a, b), возвращающую сумму двух целых неотрицательных чисел.
+
+a = int(input())
+b = int(input())
+
+def sum(a, b):
+    if b == 0:
+        return a
+    return sum(a + 1, b - 1)
+
+print(sum(a, b))
